d617cb0c3a17aff1bf602058b2d84ea4b2eda62d
Emersito25 <00247025>
2026-09-10 14:28:38 -0600

    feat: menu creado

---
+++ ejercicio.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int opcion;
+
+    cout << "== Cajero ==" << endl;
+    cout << "1. Mostrar balance" << endl;
+    cout << "2. Depositar" << endl;
+    cout << "3. Retirar" << endl;
+    cout << "4. Salir" << endl;
+    
+    cin >> opcion;
+
+
+
+    return 0;
+}
